9ea4cce9f97807e29eebb0b875abab3831561008
Adriana Lima Shikasho <adrianalimafm@gmail.com>
2022-09-05 18:04:22 -0300

    feat: remove gcp credentials

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/adriana_shikasho/gcpkey.json")
-
   project = "liquid-force-361417"
   region  = "us-west4"
   zone    = "us-west4-b"
