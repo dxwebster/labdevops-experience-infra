ef96e09a2b1bc64dc64e7efe93d4487ee0d92c7f
Adriana Lima Shikasho <adrianalimafm@gmail.com>
2022-09-06 15:45:02 -0300

    feat: change location

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,6 +1,6 @@
 resource "google_app_engine_application" "app" {
   project     = "lab-devops-cloud-xp"
-  location_id = "us-west1"
+  location_id = "us-west4"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
